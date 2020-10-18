87b86beb4bbc24ce81b7f8cd04f68e45079f7482
ibirossi <ibi.homaidan@gmail.com>
2020-10-18 10:09:15 +0200

    listing returns only users listed=true

--- Code/routes/listingRoutes.js
+++ Code/routes/listingRoutes.js
@@ -1,17 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs')
-const userModel = require('../models/User.model');
-const userDetailsModel = require('../models/UserDetails.model')
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/User.model");
+const userDetailsModel = require("../models/UserDetails.model");
 
-
-router.get('/viewAll', (req, res) => {
-    if(req.params.listed)
-    userModel.find()
-    .then((user)=>{
-        res.render('listings/viewAll', {user})
-    })
-})
-
+router.get("/viewAll/", (req, res) => {
+  userModel.find({listed: true})
+  .then((user) => {
+    res.render("listings/viewAll", { user });
+  });
+});
 
 module.exports = router;
